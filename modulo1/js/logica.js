61d71689368a26256c5f16359d2ded56deec57c3
Edily Mora <edilyrosa@gmail.com>
2025-09-25 23:48:19 -0400

    first commit

---
+++ modulo1/js/logica.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById("tbn")
+        function pintaBody(){
+            alert('hola')
+            document.body.style.backgroundColor = 'lightblue'
+        }
+        btn.addEventListener('click', pintaBody)
+   
